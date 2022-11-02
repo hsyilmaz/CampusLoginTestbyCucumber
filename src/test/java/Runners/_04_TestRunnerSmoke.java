644ac51336b051f51529998e43de2fbf6b3b7ed3
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-11-02 23:26:25 +0300

    +

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
+        tags = "@SmokeTest or @Regression", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"}
         ,plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}  // master report için sonradan ekleniyor
