b8debe6e5512ab1cad43f77dad75a8663d7f2c1f
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-11-03 11:01:43 +0300

    +

--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -6,10 +6,11 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(  // or olduğunda herhangi bir tanesi varsa çalıştırır
         // and olduğunda her ikisinin de bulundugu senaryolari calistirir.
-        tags = "@Regression or @SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
+        tags = "@Regression or @SmokeTest",
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"}
-        ,plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+        , plugin = { "html:target/cucumber-reports.html" }
+        // windows için plugin = {"html:target//cucumber-reports.html"}
 )
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
-}  // Browser tipi vermek için, rapora ek parametre eklemek için sonra kullanacağız
+}
