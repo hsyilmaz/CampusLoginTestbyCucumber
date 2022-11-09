b7e0f840e8766af82f20b89c474544963a5e1951
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-11-09 18:52:48 +0300

    Listeners class added to Runners.

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -1,8 +1,10 @@
 package Runners;
 
 
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Listeners;
 
 @CucumberOptions(
         tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
@@ -10,6 +12,7 @@ import io.cucumber.testng.CucumberOptions;
         glue = {"StepDefinitions"}
         ,plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}  // master report için sonradan ekleniyor
 )
+@Listeners({ExtentITestListenerClassAdapter.class})
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 
 }
--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -1,8 +1,10 @@
 package Runners;
 
 
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Listeners;
 
 @CucumberOptions(  // or olduğunda herhangi bir tanesi varsa çalıştırır
         // and olduğunda her ikisinin de bulundugu senaryolari calistirir.
@@ -12,5 +14,6 @@ import io.cucumber.testng.CucumberOptions;
         , plugin = { "html:target/cucumber-reports.html" }
         // windows için plugin = {"html:target//cucumber-reports.html"}
 )
+@Listeners({ExtentITestListenerClassAdapter.class})
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
 }
